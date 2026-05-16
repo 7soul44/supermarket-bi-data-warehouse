6eb724783cb003b24a2e54c97063f97546052d6a
7soul44 <dkyb44@gmail.com>
2026-05-16 18:19:16 +0300

    Add comments to 01_create_database.sql
    Added project header comments to the SQL script.

--- sql/01_create_database.sql
+++ sql/01_create_database.sql
@@ -1,3 +1,9 @@
+-- =====================================================
+-- Project: Supermarket BI Data Warehouse
+-- Script: 01_create_database.sql
+-- Purpose: Create the SQL Server database
+-- =====================================================
+
 CREATE DATABASE SupermarketDW;
 GO
 
